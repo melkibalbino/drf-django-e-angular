9850e86103efa1a36d0f7e86a2b4b260389f5b54
melki <melkibalbinoads@gmail.com>
2019-10-13 17:59:00 -0300

    Criado o primeiro endpoint da api django

--- django/todoapi/models.py
+++ django/todoapi/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Task(models.Model):
+    """Stores a task"""
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=50)
+
+    create_on = models.DateField(default=date.today)
+
+    due_date = models.DateField(default=date.today)
+
+    class Meta:
+        db_table = 'task'
+        ordering = ['id']
+    
+    def __str__(self):
+        return self.title
--- django/todoproj/urls.py
+++ django/todoproj/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todoapi.views import TaskList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/task/', TaskList.as_view(), name='task-list'),
+
 ]
